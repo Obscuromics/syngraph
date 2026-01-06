7ad7714e83b2572698a1cf860561c3434da8930f
KamilSJaron <kamiljaron@gmail.com>
2026-01-06 22:10:57 +0100

    commenting out most of the plotting script and will track addition of individual changes

---
+++ scripts/ALGs_palettes.R
@@ -0,0 +1,23 @@
+# d_ - diptera
+# db_ - brachycera
+# ds_ - syrphidae + phoridae + schizophora
+# dst_ - Stratiomyidae
+
+pal <- c("d1" = "#169e73ff", "d2" = "#e59d38ff", "d3" = "#1573afff",
+         "d4" = "#f0e354ff", "d5" = "#60b5e1ff", "d6" = "black", "100" = "white",
+         "db1a" = "#25d8a0ff", "db1b" = "#168a65ff",
+         "db2a" = "#ffbd60ff", "db2b" = "#f29717ff", 
+         "db3a" = "#1591dfff", "db3b" = "#005990ff",
+         "db4a" = "#f0e354ff", "db4b" = "#e2d119ff",
+         "db5" =  "#60b5e1ff", "db6" = "black",
+         "ds1" = "#008d17ff", "ds2" = "#60b5e1ff", "ds3" = "#988900ff",
+         "ds4" = "#faed00ff", "ds5" = "#88ca91ff", "ds6" = "black",
+         "dst1a" = "#00b386ff", "dst1b" = "#01442aff",
+         "dst2" = "#60b5e1ff", "dst3" = "#ffdd00ff",
+         "dst5" = "#f74a00ff", "dst6" = "black",
+         "y1" = "#ff00f2ff", "y2" = "#6fff00ff", "y3" = "#0e9defff",
+         "y4" = "#e6b502ff", "y5" = "#620462ff", "y6" = "#56dedeff",
+         "y7" = "#f97400ff", "y8" = "#008000ff", "y9" = "#080867ff",
+         "y10" = "#8b0a0aff", "y11" = "#657d58ff", "y12" = "#366060ff",
+         "y13" = "#e1e16fff"
+         )
